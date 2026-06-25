1c47c4818b7c8574ef7539b11e382dda8e2dc27b
EXPLUSMORE <info@explusmore.com>
2026-06-25 20:42:40 +0200

    fix: remove base path for custom domain + add CNAME file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/What-if-Doggy/',
   plugins: [
     react(),
     VitePWA({
